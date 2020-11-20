981da0d431dd54e5f39f87dc51504f0ae23ad47b
Tillmann Weimer <tillmann1970@gmail.com>
2020-11-20 13:31:52 +0100

    imported axios

--- src/App.js
+++ src/App.js
@@ -2,8 +2,17 @@ import React from "react"
 
 import Search from "./components/Search"
 import "./App.css"
+import axios from "axios"
 
 class App extends React.Component {
+  state = {
+    images: []
+  }
+  showImages() {
+    axios.get("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=b54580f369a7eeebecb2004dc429d08f&format=json&nojsoncallback=1&text=aik").then(res => {
+      this.setState({ images: res.data })
+    })
+  }
   render() {
     return <Search />
   }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,28 +1,28 @@
-import React, { useEffect } from "react"
+import React from "react"
 
 function Search() {
   return (
     <>
-      <div class="controls">
-        <div class="logo__wrapper"></div>
-        <div class="searchfield">
-          <input type="text" placeholder="Search flickr for photos..." class="searchfield__input" />
-          <button class="searchfield__submit-button">Search</button>
+      <div className="controls">
+        <div className="logo__wrapper"></div>
+        <div className="searchfield">
+          <input type="text" placeholder="Search flickr for photos..." className="searchfield__input" />
+          <button className="searchfield__submit-button">Search</button>
         </div>
-        <div class="gallery-button__wrapper">
-          <button class="gallery-button">Show Gallery</button>
+        <div className="gallery-button__wrapper">
+          <button className="gallery-button">Show Gallery</button>
         </div>
       </div>
 
-      <div class="message__container"></div>
+      <div className="message__container"></div>
 
-      <div class="searchresults"></div>
+      <div className="searchresults"></div>
 
-      <div class="gallery">
-        <div class="gallery__content">
-          <div class="gallery__controls">
-            <button class="gallery__control-left"></button>
-            <button class="gallery__control-right"></button>
+      <div className="gallery">
+        <div className="gallery__content">
+          <div className="gallery__controls">
+            <button className="gallery__control-left"></button>
+            <button className="gallery__control-right"></button>
           </div>
         </div>
       </div>
