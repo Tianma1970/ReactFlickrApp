e28f067786b89b983156808839f76a4f48250f86
Tillmann Weimer <tillmann1970@gmail.com>
2020-11-23 12:06:58 +0100

    imported Gallery

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from "react"
 
 import Search from "./components/Search"
+import Gallery from "./components/Gallery"
+
 import "./App.css"
-import axios from "axios"
 
 class App extends React.Component {
-  state = {
-    images: []
-  }
-  showImages() {
-    axios.get("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=b54580f369a7eeebecb2004dc429d08f&format=json&nojsoncallback=1&text=aik").then(res => {
-      this.setState({ images: res.data })
-    })
-  }
   render() {
-    return <Search />
+    return (
+      <>
+        <Search />
+        <Gallery />
+      </>
+    )
   }
 }
 
